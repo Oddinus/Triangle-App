fcbbbfe838f3782065024efea0cbf627014b0c79
Piotr <petruslozowski@gmail.com>
2023-03-14 11:32:59 +0100

    Final Version

--- Triangle App/Program.cs
+++ Triangle App/Program.cs
@@ -2,66 +2,63 @@
 {
     public static void Main(string[] args)
     {
+        static double ComputeSides(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+        }
+
+        static bool isPythagorean(double hypotenuse, double rectangular1, double rectangular2)
+        {
+            return Math.Abs(Math.Pow(hypotenuse, 2) - (Math.Pow(rectangular1, 2) + Math.Pow(rectangular2, 2))) <= Math.Pow(0.1, 10);
+        }
+
+        static double EnterCoords(string message)
+        {
+            Console.WriteLine(message);
+            double number;
+            while (!Double.TryParse(Console.ReadLine(), out number))
+            {
+                Console.WriteLine("Invalid number given, try again:");
+            }
+            return number;
+        }
+
         Console.WriteLine("Triangle calculator");
 
         Console.WriteLine("");
 
-        double xA, yA, xB, yB, xC, yC;
-
         //Ask user to type numbers
-        Console.WriteLine("Enter coordinate x of dot A:");
-        Double.TryParse(Console.ReadLine(), out xA);
-
-        //Invalid number check proposition
-        /* 
-        while (!Double.TryParse(Console.ReadLine(), out xA))
-        {
-            Console.WriteLine("Invalid number given, try again:");
-        }
-        */
-
-        Console.WriteLine("Enter coordinate y of dot A:");
-        Double.TryParse(Console.ReadLine(), out yA);
-
-        Console.WriteLine("Enter coordinate x of dot B:");
-        Double.TryParse(Console.ReadLine(), out xB);
-
-        Console.WriteLine("Enter coordinate y of dot B:");
-        Double.TryParse(Console.ReadLine(), out yB);
-
-        Console.WriteLine("Enter coordinate x of dot C:");
-        Double.TryParse(Console.ReadLine(), out xC);
-
-        Console.WriteLine("Enter coordinate y of dot C:");
-        Double.TryParse(Console.ReadLine(), out yC);
+        double xA = EnterCoords("Enter coordinate x of dot A:");
+        double yA = EnterCoords("Enter coordinate y of dot A:");
+        double xB = EnterCoords("Enter coordinate x of dot B:");
+        double yB = EnterCoords("Enter coordinate y of dot B:");
+        double xC = EnterCoords("Enter coordinate x of dot C:");
+        double yC = EnterCoords("Enter coordinate y of dot C:");
 
 
         Console.WriteLine("");
 
 
         //Calculate sides
-        double calcAB = Math.Sqrt(Math.Pow((xB - xA), 2) + Math.Pow((yB - yA), 2));
-        double calcBC = Math.Sqrt(Math.Pow((xC - xB), 2) + Math.Pow((yC - yB), 2));
-        double calcAC = Math.Sqrt(Math.Pow((xC - xA), 2) + Math.Pow((yC - yA), 2));
+        double a = ComputeSides(xA, yA, xB, yB);
+        double b = ComputeSides(xB, yB, xC, yC);
+        double c = ComputeSides(xA, yA, xC, yC);
 
 
         //Check triangle type
-        bool equiliteral = calcAB == calcBC && calcBC == calcAC && calcAB == calcAC;
-        bool isoscales = calcAB == calcBC || calcBC == calcAC || calcAB == calcAC;
-        bool scalene = calcAB != calcBC && calcBC != calcAC && calcAB != calcAC;
-        bool pythagorean = Math.Abs(Math.Pow(calcBC, 2) - (Math.Pow(calcAB, 2) + Math.Pow(calcAC, 2))) <= Math.Pow(0.1, 10) ||
-                            Math.Pow(calcAB, 2) + Math.Pow(calcBC, 2) == Math.Pow(calcAC, 2) ||
-                            Math.Pow(calcBC, 2) + Math.Pow(calcAC, 2) == Math.Pow(calcAB, 2) ||
-                            Math.Pow(calcAB, 2) + Math.Pow(calcAC, 2) == Math.Pow(calcBC, 2);
+        bool equiliteral = a == b && b == c && a == c;
+        bool isoscales = a == b || b == c || a == c;
+        bool scalene = a != b && b != c && a != c;
+        bool pythagorean = isPythagorean(a, b, c) || isPythagorean(b, c, a) || isPythagorean(c, a, b);
 
 
         //Show informations
-        if (calcAB != 0 && calcBC != 0 && calcAC != 0)
+        if (a != 0 && b != 0 && c != 0)
         {
             //Show sides length
-            Console.WriteLine($"Length of AB is: '{calcAB}'");
-            Console.WriteLine($"Length of BC is: '{calcBC}'");
-            Console.WriteLine($"Length of AC is: '{calcAC}'");
+            Console.WriteLine($"Length of AB is: '{a}'");
+            Console.WriteLine($"Length of BC is: '{b}'");
+            Console.WriteLine($"Length of AC is: '{c}'");
 
             Console.WriteLine("");
 
@@ -75,8 +72,8 @@
 
 
             //Calculate perimeter
-            double calcP = (calcAB + calcBC + calcAC);
-            Console.WriteLine($"Perimeter is: {calcP}");
+            double p = (a + b + c);
+            Console.WriteLine($"Perimeter is: {p}");
 
             Console.WriteLine("");
 
@@ -84,7 +81,7 @@
             //Calculate parity numbers
             Console.WriteLine("Parity numbers in range from 0 to triangle perimeter:");
 
-            for (int i = 0; i < calcP; i += 2)
+            for (int i = 0; i < p; i += 2)
             {
                 Console.WriteLine(i);
             }
